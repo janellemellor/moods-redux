8cc59d0b3542709d7eed7795327074ceee6387e8
janelle mellor <janellemellor@gmail.com>
2020-05-12 16:47:10 -0700

    add handler for drink coffee in reducer

--- src/reducers/moodsReducer.js
+++ src/reducers/moodsReducer.js
@@ -1,11 +1,16 @@
-export default function reducer(state = 
-{
-  coffees: 0,
-  snacks: 0,
-  naps: 0,
-  studies: 0
-}, action) {
+import { DRINK_COFFEE } from '../actions/moodsActions';
+
+export default function reducer(
+  state = {
+    coffees: 0,
+    snacks: 0,
+    naps: 0,
+    studies: 0
+  }, action) {
   switch(action.type) {
+    case DRINK_COFFEE:
+      return { ...state,
+        coffees: state.coffees + 1 };  
     default:
       return state;
   }
